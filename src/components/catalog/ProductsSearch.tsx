f378b66b267ada2a9fa24212af5a9d20cb6a9df7
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2026-01-27 16:42:10 +0300

    use debounce on search

--- src/components/catalog/ProductsSearch.tsx
+++ src/components/catalog/ProductsSearch.tsx
@@ -2,12 +2,24 @@
 import { Input } from "@heroui/input";
 import { SearchIcon } from "@heroui/shared-icons";
 import { useProducts } from "@/hooks/useProducts";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useDebounce";
 
 export const ProductsSearch = () => {
     const {
         searchQuery,
         onSearchChange,
     } = useProducts()
+    const [inputValue, setInputValue] = useState(searchQuery)
+    const debouncedValue  = useDebounce(inputValue, 1000)
+
+    const handleValueChange = (value: string) => {
+        setInputValue(value)
+    }
+
+    useEffect(() => {
+        onSearchChange(debouncedValue)
+    }, [debouncedValue])
 
     return (
         <Input
@@ -18,8 +30,8 @@ export const ProductsSearch = () => {
             startContent={
                 <SearchIcon/>
             }
-            value={searchQuery}
-            onValueChange={onSearchChange}
+            value={inputValue}
+            onValueChange={handleValueChange}
         />
     )
 }
